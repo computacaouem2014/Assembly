747d82e90c9b976fcc193d82e50c30a9f27f7e73
computacaouem2014 <computacaouem2014@users.noreply.github.com>
2016-05-02 16:20:12 -0300

    Create lesson3b.s

---
+++ w4ll3/lesson3b.s
@@ -0,0 +1,25 @@
+.section .data
+	output: .asciz "O ID do fabricante eh '%s'\n"
+
+.section .bss
+.lcomm fabri, 16
+
+.section .text
+.globl _start
+_start:
+	movl $0, %eax
+	cpuid
+	
+	movl $fabri, %edi
+	movl %ebx, 0(%edi)
+	movl %edx, 4(%edi)
+	movl %ecx, 8(%edi)
+
+	pushl $fabri
+	pushl $output
+	call printf
+	popl %eax
+	popl %eax
+
+	pushl $0
+	call exit
